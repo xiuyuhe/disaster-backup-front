88f03ec02f973083cf4720946437492319bb872e
xiuyuhe <xyhe@qingyuanos.com>
2017-05-14 22:30:04 +0800

    side bar

--- src/assets/js/sidebar.js
+++ src/assets/js/sidebar.js
@@ -1,18 +1,6 @@
 $(function () {
 
-	$('#sidebar-menu, #customize-menu').metisMenu({
-		activeClass: 'open'
-	});
-
-
-	$('#sidebar-collapse-btn').on('click', function(event){
-		event.preventDefault();
-		
-		$("#app").toggleClass("sidebar-open");
-	});
-
-	$("#sidebar-overlay").on('click', function() {
-		$("#app").removeClass("sidebar-open");
-	});
-	
+  $('#sidebar-menu').metisMenu({
+    activeClass: 'open'
+  });
 });
